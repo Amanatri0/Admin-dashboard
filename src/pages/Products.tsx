5d94f348a3d31d91694fd103aaa92e25b9e06fc3
Amanatri0 <atriaman8897@gmail.com>
2025-10-24 21:04:00 +0530

    Layout of product page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,11 +1,59 @@
+import { useCallback, useState, type ReactElement } from "react";
 import AdminSidebar from "../component/AdminSidebar";
+import TableHOC from "../component/TableHOC";
+import type { ColumnDef } from "@tanstack/react-table";
+
+interface Datatype {
+  photo: string;
+  name: string;
+  price: number;
+  stock: number;
+  action: ReactElement;
+}
+
+const columns: ColumnDef<Datatype>[] = [
+  {
+    accessorKey: "photo",
+    header: "Photo",
+  },
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+  },
+  {
+    accessorKey: "stock",
+    header: "Stock",
+  },
+  {
+    accessorKey: "action",
+    header: "Action",
+  },
+];
+
+const arr: Datatype[] = [];
 
 const Products = () => {
+  const [data] = useState<Datatype[]>(arr);
+
+  const Table = useCallback(() => {
+    return (
+      <TableHOC<Datatype>
+        columns={columns}
+        data={data}
+        containerClassname="dashboardProductBox"
+        heading="Products"
+      />
+    );
+  }, []);
   return (
     <div className="adminContainer">
       {/* Sidebar */}
       <AdminSidebar />
-      <main>Product</main>
+      <main>{Table()}</main>
     </div>
   );
 };
