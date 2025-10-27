00d15ca05a89bbf680327c630281cd3a53307304
Amanatri0 <atriaman8897@gmail.com>
2025-10-27 21:59:32 +0530

    added pagination without any page working

--- src/component/TableHOC.tsx
+++ src/component/TableHOC.tsx
@@ -1,15 +1,19 @@
 import {
   useReactTable,
   getCoreRowModel,
+  getSortedRowModel,
   flexRender,
   type ColumnDef,
+  type SortingState,
 } from "@tanstack/react-table";
+import { useState } from "react";
 
 interface TableProps<T extends object> {
-  columns: ColumnDef<T>[];
-  data: T[];
+  columns: ColumnDef<T>[]; // columns definition
+  data: T[]; // table data
   containerClassname?: string;
   heading?: string;
+  showPagination?: boolean;
 }
 
 function TableHOC<T extends object>({
@@ -17,11 +21,18 @@ function TableHOC<T extends object>({
   data,
   containerClassname = "",
   heading = "",
+  showPagination = true,
 }: TableProps<T>) {
+  // State for sorting
+  const [sorting, setSorting] = useState<SortingState>([]);
+
   const table = useReactTable({
     data,
     columns,
+    state: { sorting },
+    onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
   });
 
   return (
@@ -29,25 +40,35 @@ function TableHOC<T extends object>({
       {heading && <h2 className="heading">{heading}</h2>}
 
       <table className="table">
-        {/*  Table Header  */}
+        {/* Table Header */}
         <thead>
           {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
               {headerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
+                <th
+                  key={header.id}
+                  onClick={header.column.getToggleSortingHandler()}
+                  className="sortToggle"
+                >
+                  {header.isPlaceholder ? null : (
+                    <>
+                      {flexRender(
                         header.column.columnDef.header,
                         header.getContext()
                       )}
+                      {{
+                        asc: " 🔼",
+                        desc: " 🔽",
+                      }[header.column.getIsSorted() as string] ?? null}
+                    </>
+                  )}
                 </th>
               ))}
             </tr>
           ))}
         </thead>
 
-        {/*  Table Body  */}
+        {/* Table Body */}
         <tbody>
           {table.getRowModel().rows.map((row) => (
             <tr key={row.id}>
@@ -60,6 +81,27 @@ function TableHOC<T extends object>({
           ))}
         </tbody>
       </table>
+
+      {showPagination && (
+        <div className="tablePagination">
+          <button
+            disabled={!table.getCanPreviousPage}
+            onClick={table.previousPage}
+          >
+            Prev
+          </button>
+
+          <span>
+            {`${
+              table.getState().pagination.pageIndex + 1
+            } of page  ${table.getPageCount()} `}{" "}
+          </span>
+
+          <button disabled={!table.getCanNextPage} onClick={table.nextPage}>
+            Next
+          </button>
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -15,12 +15,13 @@ interface Datatype {
 
 const columns: ColumnDef<Datatype>[] = [
   {
-    accessorKey: "photo",
-    header: "Photo",
-  },
-  {
     accessorKey: "name",
     header: "Name",
+  },
+  {
+    accessorKey: "action",
+    header: "Action",
+    cell: ({ row }) => row.original.action,
   },
   {
     accessorKey: "price",
@@ -31,31 +32,84 @@ const columns: ColumnDef<Datatype>[] = [
     header: "Stock",
   },
   {
-    accessorKey: "action",
-    header: "Action",
+    accessorKey: "photo",
+    header: "Photo",
+    cell: ({ row }) => row.original.photo,
   },
 ];
 
 const img =
-  "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=804";
+  "https://yamahamusicstore.in/products-images/enlarge-image/VDN7140.webp?v=1761410324";
 
 const img2 = "https://m.media-amazon.com/images/I/514T0SvwkHL._SL1500_.jpg";
 
 const arr: Datatype[] = [
   {
-    photo: <img src={img} alt="elec" />,
-    name: "Puma Shoes Air Jordan Cook Nigga 2023",
+    name: "Yamaha PAC112VM Ice Blue Electric Guitar",
+    action: <Link to="/">Manage</Link>,
     price: 690,
     stock: 3,
-    action: <Link to="/">Manage</Link>,
+    photo: <img src={img} alt="elec" />,
   },
 
   {
-    photo: <img src={img2} alt="image" />,
-    name: "Macbook",
-    price: 232223,
-    stock: 213,
+    name: "Seb laptop",
     action: <Link to="/">Manage</Link>,
+    price: 1223,
+    stock: 213,
+    photo: <img src={img2} alt="image" />,
+  },
+  {
+    name: "Yamaha PAC112VM Ice Blue Electric Guitar",
+    action: <Link to="/">Manage</Link>,
+    price: 690,
+    stock: 3,
+    photo: <img src={img} alt="elec" />,
+  },
+
+  {
+    name: "Seb laptop",
+    action: <Link to="/">Manage</Link>,
+    price: 1223,
+    stock: 213,
+    photo: <img src={img2} alt="image" />,
+  },
+  {
+    name: "Yamaha PAC112VM Ice Blue Electric Guitar",
+    action: <Link to="/">Manage</Link>,
+    price: 690,
+    stock: 3,
+    photo: <img src={img} alt="elec" />,
+  },
+
+  {
+    name: "Seb laptop",
+    action: <Link to="/">Manage</Link>,
+    price: 1223,
+    stock: 213,
+    photo: <img src={img2} alt="image" />,
+  },
+  {
+    name: "Yamaha PAC112VM Ice Blue Electric Guitar",
+    action: <Link to="/">Manage</Link>,
+    price: 690,
+    stock: 3,
+    photo: <img src={img} alt="elec" />,
+  },
+
+  {
+    name: "Seb laptop",
+    action: <Link to="/">Manage</Link>,
+    price: 1223,
+    stock: 213,
+    photo: <img src={img2} alt="image" />,
+  },
+  {
+    name: "Yamaha PAC112VM Ice Blue Electric Guitar",
+    action: <Link to="/">Manage</Link>,
+    price: 690,
+    stock: 3,
+    photo: <img src={img} alt="elec" />,
   },
 ];
 
