679539378a435c6f0ec1a99e10fca6c5059635a6
Amanatri0 <atriaman8897@gmail.com>
2025-10-28 20:35:04 +0530

    Added the customer and transaction page using tanstakc table

--- src/pages/Customer.tsx
+++ src/pages/Customer.tsx
@@ -1,11 +1,69 @@
+import { useCallback, useState, type ReactElement } from "react";
 import AdminSidebar from "../component/AdminSidebar";
+import type { ColumnDef } from "@tanstack/react-table";
+import TableHOC from "../component/TableHOC";
+
+interface Datatype {
+  avatar: ReactElement;
+  name: string;
+  email: string;
+  gender: string;
+  role: string;
+  action: ReactElement;
+}
+
+const columns: ColumnDef<Datatype>[] = [
+  {
+    accessorKey: "avatar",
+    header: "Avatar",
+    cell: ({ row }) => row.original.avatar,
+  },
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "action",
+    header: "Action",
+    cell: ({ row }) => row.original.action,
+  },
+  {
+    accessorKey: "role",
+    header: "Role",
+  },
+  {
+    accessorKey: "gender",
+    header: "Gender",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+    cell: ({ row }) => row.original.email,
+  },
+];
+
+const arr: Datatype[] = [];
 
 const Customer = () => {
+  const [data] = useState<Datatype[]>(arr);
+
+  const Table = useCallback(() => {
+    return (
+      <TableHOC<Datatype>
+        columns={columns}
+        data={data}
+        containerClassname="dashboardProductBox"
+        heading="Customers"
+        showPagination={true}
+      />
+    );
+  }, []);
+
   return (
     <div className="adminContainer">
       {/* Sidebar */}
       <AdminSidebar />
-      <main>Customer</main>
+      <main>{Table()}</main>
     </div>
   );
 };
--- src/pages/Transaction.tsx
+++ src/pages/Transaction.tsx
@@ -1,11 +1,68 @@
+import type { ColumnDef } from "@tanstack/react-table";
 import AdminSidebar from "../component/AdminSidebar";
+import { useCallback, useState, type ReactElement } from "react";
+import TableHOC from "../component/TableHOC";
+
+interface Datatype {
+  user: string;
+  amount: number;
+  discount: number;
+  quantity: number;
+  status: string;
+  action: ReactElement;
+}
+
+const columns: ColumnDef<Datatype>[] = [
+  {
+    accessorKey: "user",
+    header: "User",
+    // cell: ({ row }) => row.original.avatar,
+  },
+  {
+    accessorKey: "amount",
+    header: "Amount",
+  },
+  {
+    accessorKey: "discount",
+    header: "Discount",
+    // cell: ({ row }) => row.original.action,
+  },
+  {
+    accessorKey: "quantity",
+    header: "Quantity",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "action",
+    header: "Action",
+    cell: ({ row }) => row.original.action,
+  },
+];
+
+const arr: Datatype[] = [];
 
 const Transaction = () => {
+  const [data] = useState<Datatype[]>(arr);
+
+  const Table = useCallback(() => {
+    return (
+      <TableHOC<Datatype>
+        columns={columns}
+        data={data}
+        containerClassname="dashboardProductBox"
+        heading="Transactions"
+        showPagination={true}
+      />
+    );
+  }, []);
   return (
     <div className="adminContainer">
       {/* Sidebar */}
       <AdminSidebar />
-      <main>Transactions</main>
+      <main>{Table()}</main>
     </div>
   );
 };
