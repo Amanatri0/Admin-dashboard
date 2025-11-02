ffb5fd75b2f3772c2979532d9a8dd760dbd5eb25
Amanatri0 <atriaman8897@gmail.com>
2025-11-02 17:34:05 +0530

    Adding transaction page

--- src/pages/management/TransactionManagement.tsx
+++ src/pages/management/TransactionManagement.tsx
@@ -1,6 +1,40 @@
+import { useState } from "react";
 import AdminSidebar from "../../component/AdminSidebar";
+import type { OrderItemType, OrderType } from "../../types";
+
+const img =
+  "https://yamahamusicstore.in/products-images/enlarge-image/VDN7140.webp?v=1761410324";
+
+const orderItems: OrderItemType[] = [
+  {
+    name: "Yamaha guitar",
+    photo: img,
+    _id: "123456678",
+    quantity: 4,
+    price: 200,
+  },
+];
 
 const TransactionManagement = () => {
+  const [order, setOrder] = useState<OrderType>({
+    name: "Aman",
+    address: "Gandhi",
+    city: "Guwahati",
+    state: "Assam",
+    country: "Bharat",
+    pinCode: 1231221,
+    status: "Processing",
+    subtotal: 3000,
+    discount: 1000,
+    shippingCharges: 0,
+    tax: 200,
+    total: 3000 + 200 + 0 - 1000,
+    orderItems,
+    _id: "1w1w1w2sws2",
+  });
+
+  const {} = order;
+
   return (
     <div className="adminContainer">
       <AdminSidebar />
